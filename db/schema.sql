9405100f81fa8c6ef4c4eddf2c2c0c0613c123d7
Matt Fewer <mattfewer@hotmail.com>
2018-03-19 00:05:53 +0100

    sendFriendRequest working

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,17 @@
-DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS friendships;
 
-CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
-    firstname VARCHAR(50),
-    lastname VARCHAR(50),
-    email VARCHAR(100),
-    username VARCHAR(30),
-    password VARCHAR(100),
-    profilepic TEXT,
-    bio TEXT,
-    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- CREATE TABLE users(
+--     id SERIAL PRIMARY KEY,
+--     firstname VARCHAR(50),
+--     lastname VARCHAR(50),
+--     email VARCHAR(100),
+--     username VARCHAR(30),
+--     password VARCHAR(100),
+--     profilepic TEXT,
+--     bio TEXT,
+--     create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
 
 CREATE TABLE friendships (
     id SERIAL PRIMARY KEY,
