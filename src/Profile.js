2e8499233bb9dd4560409afb979e610059f69bd9
Matt Fewer <mattfewer@hotmail.com>
2018-06-22 14:38:17 +0200

    new styled upload form

--- src/Profile.js
+++ src/Profile.js
@@ -38,7 +38,22 @@ class Profile extends Component {
     }
 
     handleFileChange(e) {
-        this.setState({ [e.target.name]: e.target.files[0] })
+        var fileName = '';
+        if( this.files && this.files.length > 1 ){
+            fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length )
+        } else {
+            fileName = e.target.value.split( '\\' ).pop()
+        }
+
+        if( fileName ) {
+            this.label.querySelector( 'span' ).innerHTML = fileName
+        } else {
+            this.label.innerHTML = labelVal
+        }
+
+        this.setState({ [e.target.name]: e.target.files[0] }, () => {
+            e.target.value = ''
+        })
     }
 
     toggleBio() {
@@ -58,10 +73,20 @@ class Profile extends Component {
                     <div className="profile-left">
 
                         { this.state.showUploadImage &&
-                            <form onSubmit={ this.handleSubmitImg }>
-                                <input onChange={ this.handleFileChange } type="file" placeholder="upload an image" name="profilepic" />
-                                <button type="submit">Submit</button>
-                            </form>
+                            <div id="upload-modal">
+                                <div id="overlay" onClick={ this.toggleShowUploadImage }></div>
+                                <div id="upload-form-container">
+                                    <h3>Choose a profile picture</h3>
+                                    <form onSubmit={ this.handleSubmitImg }>
+                                        <input id="image-upload-input" onChange={ this.handleFileChange } type="file" placeholder="upload an image" name="profilepic" data-multiple-caption="{count} files selected" multiple />
+                                        <label htmlFor="image-upload-input" ref={ elem => this.label = elem }>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"></path></svg>
+                                            <span>Choose File</span>
+                                        </label>
+                                        <button type="submit">Submit</button>
+                                    </form>
+                                </div>
+                            </div>
                         }
                     </div>
 
